c1992aed74e8b1c9bbd167b746e995c4b901ecb9
kissinggramy <salmon7120blue@gmail.com>
2020-07-06 03:30:47 +0900

    ステージ描画

---
+++ C-2Period-end-report/C-2Period-end-report/main.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include "curses.h"
+#include <vector>
+#include <stdio.h>
+#include "Setting.h"
+#include "stage.h"
+#define STAGE_W 10
+#define STAGE_H 20
+#define CHARBUFF 124 
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;i++)
+//windowsize=192×50
+//stagesize=20×40
+
+/*
+	メインファイル
+*/
+int main()
+{
+	//外部変数
+	extern vector<vector<int>> stage;
+	//変数
+	int H, W;//ウィンドウサイズ
+	P stage_pivot;
+	stage_pivot.x=STAGE_PIVOT_X;
+	stage_pivot.y = STAGE_PIVOT_Y;
+	//現在のディレクトリ
+	char curdir[CHARBUFF];
+	getCurrentDirectory(curdir);
+	//setting.ini
+	char setf[CHARBUFF];
+	sprintf_s(setf, "%s\\setting.ini", curdir);
+	const char* section= "Stage1";
+	char Stage1f[CHARBUFF];//ファイル名
+	readChar(section, "In", "NotFound", Stage1f, setf);
+	//printf("%s\n", Stage1f);ステージファイル取得
+
+	/*rep(i, STAGE_H) {
+		rep(j, STAGE_W) {
+			cout << stage[i][j]<<" ";
+		}
+		cout << endl;
+	}*///ステージデータ取得
+	init_stage(Stage1f);//
+
+	initscr();//処理開始
+	noecho();//キー入力した文字の非表示モード
+	curs_set(0);//カーソル非表示
+	cbreak();//エンターキー不要の入力モード
+	keypad(stdscr, TRUE);//特殊キー有効化
+	start_color();//色の準備
+	init_pair(1, COLOR_WHITE, COLOR_WHITE);//壁(白色ブロック)
+	init_pair(2, COLOR_BLACK, COLOR_RED);//発電機-off(G)と、出口-off(E)
+	init_pair(3, COLOR_BLACK, COLOR_YELLOW);//発電機-on(G)
+	init_pair(4, COLOR_BLACK, COLOR_GREEN);//出口-on(E)
+	init_pair(5, COLOR_GREEN, COLOR_BLACK);//プレイヤー-安全(P)
+	init_pair(6, COLOR_YELLOW, COLOR_BLACK);//プレイヤー-注意(P)
+	init_pair(7, COLOR_RED, COLOR_BLACK);//プレイヤー-危険(P)
+	init_pair(8, COLOR_BLACK, COLOR_BLACK);//幽霊(g)
+	getmaxyx(stdscr, H, W);//ウィンドウの幅と高さ取得
+	show_stage(stage_pivot);
+	//終了
+	getch();
+	endwin();
+	return (0);
+}
--- C-2Period-end-report/C-2Period-end-report/stage.cpp
+++ C-2Period-end-report/C-2Period-end-report/stage.cpp
@@ -2,14 +2,15 @@
 #include "curses.h"
 #include "Setting.h"
 #include <vector>
+#include "stage.h"
 #define BUFFSIZE 1024
-#define STAGE_W 10
+#define STAGE_W 40
 #define STAGE_H 20
 using namespace std;
 #define rep(i,n) for(int i=0;i<n;i++)
 char* str_tok_s(char* data, const char* delim, char** ctx);
 vector<vector<int>> stage(STAGE_H, vector<int>(STAGE_W));
-void read_stage(const char* file_name) {
+void init_stage(const char* file_name) {
 	FILE* fp;
 	char s[BUFFSIZE];
 	errno_t error;
@@ -36,7 +37,37 @@ void read_stage(const char* file_name) {
 		fclose(fp);
 	}
 }
-
+void show_stage(P p) {
+	/*start_color();//色の準備
+	init_pair(1, COLOR_WHITE, COLOR_WHITE);//壁(白色ブロック)
+	init_pair(2, COLOR_BLACK, COLOR_RED);//発電機-off(G)と、出口-off(E)
+	init_pair(3, COLOR_BLACK, COLOR_YELLOW);//発電機-on(G)
+	init_pair(4, COLOR_BLACK, COLOR_GREEN);//出口-on(E)
+	init_pair(5, COLOR_GREEN, COLOR_BLACK);//プレイヤー-安全(P)
+	init_pair(6, COLOR_YELLOW, COLOR_BLACK);//プレイヤー-注意(P)
+	init_pair(7, COLOR_RED, COLOR_BLACK);//プレイヤー-危険(P)
+	init_pair(8, COLOR_BLACK, COLOR_BLACK);//幽霊(g)*/
+	rep(i, STAGE_H) {
+		rep(j, STAGE_W) {
+			if (stage[i][j] == 1) {//壁を描画する。
+				attrset(COLOR_PAIR(1));
+				mvaddstr(p.y + i, p.x + j,".");
+			}
+			else if (stage[i][j] == 2) {//プレイヤー
+				attrset(COLOR_PAIR(5));
+				mvaddstr(p.y + i, p.x + j, "P");
+			}
+			else if (stage[i][j] == 3) {//発電機
+				attrset(COLOR_PAIR(2));
+				mvaddstr(p.y + i, p.x + j, "G");
+			}
+			else if (stage[i][j] == 4) {//出口
+				attrset(COLOR_PAIR(2));
+				mvaddstr(p.y + i, p.x + j, "E");
+			}
+		}
+	}
+}
 char* str_tok_s(char* data, const char* delim, char** ctx) {
 	static char* tmp;
 	bool flg = false;
